68f2dd0d535637b521875e27b6546dca632b0e0b
ModestSurmach <mosurmach@yandex.ru>
2023-03-06 16:42:32 +0300

    Start

---
+++ Lesson2/Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Modest Surmach on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
